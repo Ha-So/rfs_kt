0b77f1d56465a389a4c44f9547e8327b895c43c1
ha-so <haris.sohl@gmail.com>
2019-09-12 13:49:37 -0400

    Added Second activity

---
+++ app/src/main/java/com/haso/rollforsushi/HubActivity.kt
@@ -0,0 +1,15 @@
+package com.haso.rollforsushi
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+
+
+class HubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hub)
+    }
+
+}
--- app/src/main/java/com/haso/rollforsushi/MainActivity.kt
+++ app/src/main/java/com/haso/rollforsushi/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.haso.rollforsushi
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.support.v4.app.SupportActivity
+import android.support.v4.app.SupportActivity.ExtraData
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var btn_new_activity = findViewById<Button>(R.id.button_get_lunch)
+
+        btn_new_activity .setOnClickListener {
+            val intent = Intent(this, HubActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
+
     }
 }
