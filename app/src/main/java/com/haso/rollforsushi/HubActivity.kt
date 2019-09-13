31040e195c01b34c6815f43b3535e3774f3c099a
ha-so <haris.sohl@gmail.com>
2019-09-13 16:36:52 -0400

    Added Second activity

--- app/src/main/java/com/haso/rollforsushi/HubActivity.kt
+++ app/src/main/java/com/haso/rollforsushi/HubActivity.kt
@@ -1,8 +1,10 @@
 package com.haso.rollforsushi
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+
 
 
 class HubActivity : AppCompatActivity() {
@@ -10,6 +12,17 @@ class HubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hub)
+
+
+
+
+        var btn_new_activity = findViewById<Button>(R.id.button_favorites)
+
+        btn_new_activity .setOnClickListener {
+            val intent = Intent(this, FavoritesActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
     }
 
 }
--- app/src/main/java/com/haso/rollforsushi/MainActivity.kt
+++ app/src/main/java/com/haso/rollforsushi/MainActivity.kt
@@ -4,11 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.support.v4.app.SupportActivity
-import android.support.v4.app.SupportActivity.ExtraData
-import android.support.v4.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.view.View
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
 
         var btn_new_activity = findViewById<Button>(R.id.button_get_lunch)
